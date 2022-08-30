eb4fc0039b302dc112603f879b7ab97146d13e07
Goketech <modupe775@gmail.com>
2022-08-30 14:47:18 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>NETFLIX</h1>
+        <div>
+            <button>Sign In</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
